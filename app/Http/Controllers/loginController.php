26762aba20a6b3253286cee2e1805b0e3fd51b5b
セイビル ムハンマド <sabilmicrosoft12@gmail.com>
2024-12-01 10:29:37 +0700

    web resto

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class LoginController extends Controller
+class loginController extends Controller
 {
     // Menampilkan form login
     public function showLoginForm()
